2b46b18ecfb2dc607fc6f62e0ca8b056f5f3a76e
fayas kp <fayaskpktr@gmail.com>
2026-03-28 07:51:27 +0530

    fix: order model enum, and header

---
+++ src/models/orderModel.js
@@ -0,0 +1,102 @@
+import mongoose from "mongoose"
+
+const itemsSchema = mongoose.Schema({
+    productId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Product"
+    },
+    variantId:{
+        type:mongoose.Schema.Types.ObjectId
+    },
+    productName:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    quantity:{
+        type:Number,
+        default:1
+    },
+    itemTotal:{
+        type:Number,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    variantAttributes: {
+        type: Map,
+        of: String
+    },
+    itemStatus: {
+        type: String,
+        enum: ["active", "cancelled","delivered", "returned"],
+        default: "active"
+    },
+    cancelReason: { type: String },
+    returnReason: { type: String },
+    comments:{type:String}
+})
+
+
+const orderSchema=mongoose.Schema({
+    orderNumber:{
+        type:String,
+        required:true
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    shippingAddress: {
+        fullName: { type: String, required: true },
+        phone: { type: String, required: true },
+        addressLine: { type: String, required: true },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        pincode: { type: String, required: true },
+        country: { type: String, default: "India" }
+    },
+    pricing:{
+        subTotal:{type:Number,required:true},
+        tax:{type:Number,default:0},
+        shipping:{type:Number,default:0},
+        discount:{type:Number,default:0},
+        total:{type:Number,required:true}
+
+    },
+    orderStatus:{
+        type:String,
+        enum:["pending","processing","shipped","delivered","cancelled"],
+        default:"pending"
+    },
+    cancelReason:{
+        type:String,
+        default:""
+    },
+    paymentMethod:{
+        type:String,
+        enum:["cash","rozorpay","wallet"]
+    },
+    paymentStatus:{
+        type:String,
+        enum: ["pending", "paid", "failed", "refunded"],
+        default: "pending"
+    },
+    items:[itemsSchema],
+    couponId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"coupon",
+        default:null
+    }
+},
+{
+    timestamps:true
+}
+)
+
+export default mongoose.model("order",orderSchema);
